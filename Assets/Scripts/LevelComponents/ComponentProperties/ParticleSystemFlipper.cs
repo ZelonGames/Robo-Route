867227934ff80a8742ab3e2ae6a2d7709510c717
ZelonGames <35749101+ZelonGames@users.noreply.github.com>
2023-04-21 16:19:24 +0200

    Animated portals and changed texture settings

---
+++ Assets/Scripts/LevelComponents/ComponentProperties/ParticleSystemFlipper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteFlipper))]
+public class ParticleSystemFlipper : MonoBehaviour
+{
+    [SerializeField] private new ParticleSystem particleSystem;
+    
+    private SpriteFlipper spriteFlipper;
+
+    public void Start()
+    {
+        spriteFlipper = gameObject.GetComponent<SpriteFlipper>();
+
+        Flip();
+    }
+
+    public void OnValidate()
+    {
+        Flip();
+    }
+
+    private void Flip()
+    {
+        if (particleSystem != null && spriteFlipper != null)
+        {
+            if (spriteFlipper.isFlipped)
+            {
+
+            }
+            particleSystem.transform.localPosition = new Vector2(
+                particleSystem.transform.localPosition.x * (spriteFlipper.isFlipped ? -1 : 1),
+                particleSystem.transform.localPosition.y);
+        }
+    }
+}
